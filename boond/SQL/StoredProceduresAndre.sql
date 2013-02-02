d38966c1d9b5c8a0b85f9f8badcdfcf2216de35b
Andre Encarnacao <andreencar@gmail.com>
2013-02-02 17:11:35 +0000

    oups...

--- boond/SQL/StoredProceduresAndre.sql
+++ boond/SQL/StoredProceduresAndre.sql
@@ -9,3 +9,24 @@ INSERT INTO Person_Category VALUES(@userId, @favcat2)
 INSERT INTO Person_Category VALUES(@userId, @favcat3)
 INSERT INTO Person_Category VALUES(@userId, @favcat4)
 INSERT INTO Person_Category VALUES(@userId, @favcat5)
+
+IF OBJECT_ID ( 'dbo.BattleFavCats' ) IS NOT NULL 
+    DROP PROCEDURE dbo.BattleFavCats;
+GO
+CREATE PROCEDURE BattleFavCats (@userId int)
+AS
+Declare @catToBePlayed int
+SELECT  TOP 1 @catToBePlayed= CatID FROM Person_Category
+ORDER BY NEWID()
+PRINT(@catToBePlayed)
+
+
+
+EXEC addFavCats 2,1,2,3,4,5
+
+DELETE FROM Person_Category WHERE PersonID=2
+
+CREATE CLUSTERED INDEX toTestone
+ON Person_Category (PersonID)
+
+INSERT INTO Person_Category VALUES(2, 1)
