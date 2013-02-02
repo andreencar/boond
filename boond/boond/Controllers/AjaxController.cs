274a6e66efcd3eb4f279a472d0806144d52b587d
Andre Encarnacao <andreencar@gmail.com>
2013-02-02 15:02:32 +0000

    First Commit

---
+++ boond/boond/Controllers/AjaxController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace boond.Controllers
+{
+    public class AjaxController : Controller
+    {
+        [Authorize]
+        [HttpPost]
+        public ActionResult GetNextBattle(int categoryID ) {
+
+            return Json(new { foo = "bar" });
+        }
+
+        public ActionResult GetNextOpponent(int categoryID, int winnerID){
+
+            return Json(new { foo = "bar", baz = "Blech" });
+        }
+    }
+}
---
+++ boond/boond/Models/BattleModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace boond.Models
+{
+    public class BattleModel
+    {
+    }
+}
